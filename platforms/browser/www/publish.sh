5d948c3c4996ea394dd16166a080ef9f09882d72
bozdoz <howaboutben@gmail.com>
2017-01-13 02:32:55 -0400

    cleaning up silly commits;

--- platforms/browser/www/publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-VERSION=$(node --eval "console.log(require('./package.json').version);")
-
-# publish master branch
-git commit -am "v$VERSION"
-git tag v$VERSION -f
-git push --tags -f
-git push
